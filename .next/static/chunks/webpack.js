75a388150ae9ff878c2d03a4acb94e9e71c5b68b
SuraZz007 <surajdev9886@gmail.com>
2022-03-17 10:57:36 +0530

    create backend sanity

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6ea87c2e3e66a765"; }
+/******/ 		__webpack_require__.h = function() { return "edd452b0eb2295e0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
